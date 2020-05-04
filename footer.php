dda777e9e48ca10ad9747f4c8b234aa011036e1f
Sam Brunno <sam.brunno@isobar.com>
2020-05-04 10:52:27 +1000

    - fixed up footer so its sticky

--- footer.php
+++ footer.php
@@ -16,10 +16,15 @@
         <?php do_action( 'foundationpress_before_footer' ); ?>
         <div class="row">
           <div class="small-12 medium-9 columns text-center medium-text-left">
-            <?php dynamic_sidebar( 'footer-widgets' ); ?>
+						<div class="large-12 columns textwidget">
+							<p>See our inspirational work:<br>
+								Isobar work: <a href="https://www.isobar.com/global/en/work/" target="_blank" rel="noopener noreferrer"><strong>Click here</strong></a> | McGarryBowen work: <a href="http://www.mcgarrybowen.com/" target="_blank" rel="noopener noreferrer"><strong>Click here</strong></a>
+							</p>
+						</div>
           </div>
           <div class="small-12 medium-3 columns text-center medium-text-right">
             <a href="mailto:Brandon.Lucas@isobar.com?Subject=Dentsu%20Creative%20Inspiration%20Inquiry">CONTACT US</a>
+						<p><em><strong>Powered by <a href="https://isobaracademy.com/">Dentsu Creative Inspiration</a></strong></em></p>
           </div>
         </div>
         <?php do_action( 'foundationpress_after_footer' ); ?>
